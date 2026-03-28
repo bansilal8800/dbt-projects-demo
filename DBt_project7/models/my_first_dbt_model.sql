e7821d66bda4cacd56cf66f4ed91eb9ad6b358ac
bansilal8800 <bansilal.keloth@anblicks.com>
2026-03-28 05:24:16 +0000

    demo

--- DBt_project7/models/my_first_dbt_model.sql
+++ DBt_project7/models/my_first_dbt_model.sql
@@ -1,10 +1,11 @@
 {{ config(materialized='table') }}
 
   with source_data as (
-      select 1 as id
+      select 1 as id, 2 as id2
       union all
-      select null as id
+      select null as id , 2 as id2
   )
 
+  ---
   select *
   from source_data
